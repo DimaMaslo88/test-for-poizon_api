09cffd1c55c5d62f8870992d61a0c0ad40e9a8a4
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-08-11 11:35:06 +0300

    "feat/ui logic"

--- src/ui/mainComponent/MainComponent.tsx
+++ src/ui/mainComponent/MainComponent.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectText } from 'bll/selectors';
-import { TextReducerType } from 'bll/types/text-reducer-type';
+import style from 'ui/styles/mainContent-style/mainContent.module.css'
+import { SyncOutlined } from '@ant-design/icons';
 
 export const MainComponent = React.memo(() => {
+  const [time,setTime] = useState<number>(30)
   const mainText = useSelector(selectText);
-
+  const randomText = mainText[Math.floor(Math.random() * mainText.length)];
+  console.log(randomText);
   return (
-    <div>
-      {mainText.map(
-        ({id,text}: TextReducerType) => (
-          <div key={id}>
-            {text}
-          </div>
-        )
-      )}
+    <div className={style.mainContent}>
+      <div className={style.time}>
+        Осталось времени : {time}
+      </div>
+      <div className={style.text}>
+        {randomText.text}
+      </div>
+      <div className={style.resetIconContainer}>
+        <SyncOutlined className={style.resetIcon} />
+      </div>
+
+
     </div>
   );
 
